9a640417e2550bc5ff4d5e7427aed8286a08d053
Jumadi <54573301+jumadi-cloud@users.noreply.github.com>
2023-11-27 14:25:08 +0700

    Update proses.py

--- proses.py
+++ proses.py
@@ -7,13 +7,13 @@ import numpy as np
 
 def set_background(image_file):
     """
-    This function sets the background of a Streamlit app to an image specified by the given image file.
+    Fungsi ini mengatur latar belakang aplikasi Streamlit ke gambar yang ditentukan oleh file gambar tertentu.
 
-    Parameters:
-        image_file (str): The path to the image file to be used as the background.
+    Parameter:
+        image_file (str): Jalur ke file gambar yang akan digunakan sebagai latar belakang.
 
-    Returns:
-        None
+    Pengembalian:
+        Tidak ada
     """
     with open(image_file, "rb") as f:
         img_data = f.read()
@@ -31,16 +31,15 @@ def set_background(image_file):
 
 def classify(image, model, class_names):
     """
-    This function takes an image, a model, and a list of class names and returns the predicted class and confidence
-    score of the image.
+    Fungsi ini mengambil gambar, model, dan daftar nama kelas serta mengembalikan kelas prediksi dan skor gambar.
 
-    Parameters:
-        image (PIL.Image.Image): An image to be classified.
-        model (tensorflow.keras.Model): A trained machine learning model for image classification.
-        class_names (list): A list of class names corresponding to the classes that the model can predict.
+    Parameter:
+        image (PIL.Image.Image): Gambar yang akan diklasifikasikan.
+        model (tensorflow.keras.Model): Model pembelajaran mesin terlatih untuk klasifikasi gambar.
+        class_names (daftar): Daftar nama kelas yang sesuai dengan kelas yang dapat diprediksi oleh model.
 
-    Returns:
-        A tuple of the predicted class name and the confidence score for that prediction.
+    Pengembalian:
+        Tuple nama kelas yang diprediksi dan skor keyakinan untuk prediksi tersebut.
     """
     # convert image to (224, 224)
     image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
